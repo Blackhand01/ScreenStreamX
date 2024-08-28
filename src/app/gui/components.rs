cbcf8e7d8a89e9f79604f5be3590a09a8a4a50c3
Blackhand01 <stefano2001roy@gmail.com>
2024-08-28 18:28:50 +0200

    Added caster screen

--- src/app/gui/components.rs
+++ src/app/gui/components.rs
@@ -3,17 +3,21 @@ use crate::app::gui::caster_ui;
 use crate::app::gui::receiver_ui;
 use super::app_main::MyApp;
 
-/// Funzione principale per il rendering della UI del Caster
+// Funzione principale per il rendering della UI del Caster
 pub fn render_caster_ui(ui: &mut egui::Ui, app: &mut MyApp) {
     ui.group(|ui| {
         ui.label(egui::RichText::new("Caster mode selected").strong());
         ui.add_space(10.0);
 
         caster_ui::render_capture_area_button(ui, app);
-        caster_ui::render_annotation_toggle_button(ui, app);
         caster_ui::render_multi_monitor_support_button(ui, app);
         caster_ui::render_broadcast_button(ui, app); // Pulsante per gestire il broadcasting
         caster_ui::render_record_button(ui, app); // Pulsante per gestire la registrazione
+
+        ui.add_space(20.0);
+        ui.separator();
+
+        caster_ui::render_caster_preview(ui, app); // Nuovo pannello per l'anteprima del casting
     });
 }
 
